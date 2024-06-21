28178b8aabfb8f882153003d0421f2448fa9957d
ManuelFerro <142249191+ManuelFerro@users.noreply.github.com>
2024-06-20 21:34:01 -0300

    Update Plantel.java

--- Plantel.java
+++ Plantel.java
@@ -1,19 +1,20 @@
 package aed;
+
 public class Plantel {
 
-    // Atributo que representa la cantidad de docentes de cada tipo
+    // atributo que representa la cantidad de docentes de cada tipo
     private int[] docentes;
 
-    // Constructor
-    public Plantel(int AY2, int AY1, int JTP, int Prof) {
-        this.docentes = new int[4];
-        this.docentes[0] = AY2; // Ayudantes de segunda
-        this.docentes[1] = AY1; // Ayudantes de primera
-        this.docentes[2] = JTP; // Jefes de trabajos prácticos
-        this.docentes[3] = Prof; // Profesores
+    // constructor del plantel con datos de entrada
+    public Plantel(int cantidadAY2, int cantidadAY1, int cantidadJTP, int cantidadProf) {
+        docentes = new int[4];
+        docentes[0] = cantidadAY2; // ayudantes de segunda
+        docentes[1] = cantidadAY1; // ayudantes de primera
+        docentes[2] = cantidadJTP; // jefes de trabajos prácticos
+        docentes[3] = cantidadProf; // profesores
     }
 
-    // Método para calcular el cupo
+    // método para calcular el cupo
     public int cupo() {
         return (docentes[0] * 30) + (docentes[1] * 20) + (docentes[2] * 100) + (docentes[3] * 250);
     }
