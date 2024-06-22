77b7c501775e5e1ed267f127fc0359cdfaefb6a8
ManuelFerro <142249191+ManuelFerro@users.noreply.github.com>
2024-06-22 19:50:42 -0300

    Update Materia.java

--- Materia.java
+++ Materia.java
@@ -1,9 +1,13 @@
 package aed;
 import aed.SistemaSIU.materias_c;
 
+// clase Materia. Abstrae el concepto d euna materia de la facultad, con
+// su plantel docente (que define su cupo), los alumnos inscriptos a la misma y
+// el diccionario de todos sus nombres y en qué carrera están
+
 public class Materia {
     private ListaEnlazada<String> estudiantesAnotados;
-    private Trie<materias_c> nombresMateria;
+    private Trie<materias_c> nombresMateria; // sus valores son materias_c, un diccionario que representa el conjunto de materias de una carrera
     private Plantel plantel;
 
     // constructor de Materia vacía
