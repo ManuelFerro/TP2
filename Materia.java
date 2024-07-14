8c9bf7c0b2c2b57d515be99db43feb40c28212dd
ManuelFerro <142249191+ManuelFerro@users.noreply.github.com>
2024-07-14 13:07:13 -0300

    Update Materia.java

--- Materia.java
+++ Materia.java
@@ -1,19 +1,22 @@
 package aed;
 import aed.SistemaSIU.conjuntoMaterias;
 
-// clase Materia. Abstrae el concepto d euna materia de la facultad, con
+// clase Materia. Abstrae el concepto de una materia de la facultad, con
 // su plantel docente (que define su cupo), los alumnos inscriptos a la misma y
 // el diccionario de todos sus nombres y en qué carrera están
 
 public class Materia {
-    private ListaEnlazada<String> estudiantesAnotados;
-    private Trie<conjuntoMaterias> nombresMateria; // sus valores son conjuntoMaterias, un diccionario que representa el conjunto de materias de una carrera
     private Plantel plantel;
+    private ListaEnlazada<String> estudiantesAnotados;
+    private Trie<ListaEnlazada<conjuntoMaterias>> nombresMateria;
+    // sus valores son listas de conjuntoMaterias, diccionarios que representan el conjunto de materias de una carrera
+    // están en listas enlazadas para permitir que varias carreras se refieran a una materia con el mismo nombre,
+    // agregando o quitando mediante punteros, lo que mantiene una complejidad de O(1)
 
     // constructor de Materia vacía
     public Materia(){
         estudiantesAnotados = new ListaEnlazada<>();
-        nombresMateria = new Trie<conjuntoMaterias>();
+        nombresMateria = new Trie<ListaEnlazada<conjuntoMaterias>>();
         plantel = new Plantel();
     }
 
@@ -21,7 +24,7 @@ public class Materia {
         return estudiantesAnotados;
     }
 
-    public Trie<conjuntoMaterias> nombresMateria(){
+    public Trie<ListaEnlazada<conjuntoMaterias>> nombresMateria(){
         return nombresMateria;
     }
 
